8dd67cdeb68c31bb936b567631d126ad02d06b64
blackstar147.chen <blackstar147.chen@huolala.cn>
2021-11-09 19:57:49 +0800

    add deepfm init

--- deepfm.py
+++ deepfm.py
@@ -5,9 +5,20 @@ import time
 import numpy as np
 import pandas as pd
 import tensorflow.compat.v1 as tf
+# import tensorflow as tf
+from keras.losses import binary_crossentropy
+from keras.metrics import AUC
+from keras.optimizer_v2.adam import Adam
+
+from tensorflow.keras import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.layers import Embedding, Dropout, Dense, Input, Layer
+
 from tensorflow import feature_column as fc
 from comm import ACTION_LIST, STAGE_END_DAY, FEA_COLUMN_LIST
 from evaluation import uAUC, compute_weighted_score
+
+
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -22,20 +33,133 @@ flags.DEFINE_float('embed_l2', None, 'embedding l2 reg')
 SEED = 2021
 
 
-class WideAndDeep(object):
+class FM(Layer):
+    """
+    Wide part
+    """
 
-    def __init__(self, linear_feature_columns, dnn_feature_columns, stage, action):
+    def __init__(self, feature_length, w_reg=1e-6):
+        """
+        Factorization Machine
+        In DeepFM, only the first order feature and second order feature intersect are included.
+        :param feature_length: A scalar. The length of features.
+        :param w_reg: A scalar. The regularization coefficient of parameter w.
+        """
+        super(FM, self).__init__()
+        self.feature_length = feature_length
+        self.w_reg = w_reg
+
+    def build(self, input_shape):
+        self.w = self.add_weight(name='w', shape=(self.feature_length, 1),
+                                 initializer='random_normal',
+                                 regularizer=l2(self.w_reg),
+                                 trainable=True)
+
+    def call(self, inputs, **kwargs):
+        """
+        :param inputs: A dict with shape `(batch_size, {'sparse_inputs', 'embed_inputs'})`:
+          sparse_inputs is 2D tensor with shape `(batch_size, sum(field_num))`
+          embed_inputs is 3D tensor with shape `(batch_size, fields, embed_dim)`
+        """
+        sparse_inputs, embed_inputs = inputs['sparse_inputs'], inputs['embed_inputs']
+        # first order
+        first_order = tf.reduce_sum(tf.nn.embedding_lookup(self.w, sparse_inputs), axis=1)  # (batch_size, 1)
+        # second order
+        square_sum = tf.square(tf.reduce_sum(embed_inputs, axis=1, keepdims=True))  # (batch_size, 1, embed_dim)
+        sum_square = tf.reduce_sum(tf.square(embed_inputs), axis=1, keepdims=True)  # (batch_size, 1, embed_dim)
+        second_order = 0.5 * tf.reduce_sum(square_sum - sum_square, axis=2)  # (batch_size, 1)
+        return first_order + second_order
+
+
+class DNN(Layer):
+    """
+    Deep part
+    """
+
+    def __init__(self, hidden_units, activation='relu', dnn_dropout=0.):
+        """
+        DNN part
+        :param hidden_units: A list like `[unit1, unit2,...,]`. List of hidden layer units's numbers
+        :param activation: A string. Activation function.
+        :param dnn_dropout: A scalar. dropout number.
+        """
+        super(DNN, self).__init__()
+        self.dnn_network = [Dense(units=unit, activation=activation) for unit in hidden_units]
+        self.dropout = Dropout(dnn_dropout)
+
+    def call(self, x, **kwargs):
+        for dnn in self.dnn_network:
+            x = dnn(x)
+        x = self.dropout(x)
+        return x
+
+
+class DeepFM(Model):
+    def __init__(self, feature_columns, hidden_units=(200, 200, 200), dnn_dropout=0.,
+                 activation='relu', fm_w_reg=1e-6, embed_reg=1e-6):
+        """
+            DeepFM
+            :param feature_columns: A list. sparse column feature information.
+            :param hidden_units: A list. A list of dnn hidden units.
+            :param dnn_dropout: A scalar. Dropout of dnn.
+            :param activation: A string. Activation function of dnn.
+            :param fm_w_reg: A scalar. The regularizer of w in fm.
+            :param embed_reg: A scalar. The regularizer of embedding.
+        """
+        super(DeepFM, self).__init__()
+        self.sparse_feature_columns = feature_columns
+        self.embed_layers = {
+            'embed_' + str(i): Embedding(input_dim=40000,
+                                         input_length=1,
+                                         output_dim=200,
+                                         embeddings_initializer='random_normal',
+                                         embeddings_regularizer=l2(embed_reg))
+            for i, feat in enumerate(self.sparse_feature_columns)
+        }
+        self.index_mapping = []
+        self.feature_length = 0
+        for feat in self.sparse_feature_columns:
+            self.index_mapping.append(self.feature_length)
+            self.feature_length += 40000
+        self.embed_dim = 200  # all sparse features have the same embed_dim
+        self.fm = FM(self.feature_length, fm_w_reg)
+        self.dnn = DNN(hidden_units, activation, dnn_dropout)
+        self.dense = Dense(1, activation=None)
+
+    def call(self, inputs, **kwargs):
+        sparse_inputs = inputs
+        # embedding
+        sparse_embed = tf.concat([self.embed_layers['embed_{}'.format(i)](sparse_inputs[:, i])
+                                  for i in range(sparse_inputs.shape[1])], axis=-1)  # (batch_size, embed_dim * fields)
+        # wide
+        sparse_inputs = sparse_inputs + tf.convert_to_tensor(self.index_mapping)
+        wide_inputs = {'sparse_inputs': sparse_inputs,
+                       'embed_inputs': tf.reshape(sparse_embed, shape=(-1, sparse_inputs.shape[1], self.embed_dim))}
+        wide_outputs = self.fm(wide_inputs)  # (batch_size, 1)
+        # deep
+        deep_outputs = self.dnn(sparse_embed)
+        deep_outputs = self.dense(deep_outputs)  # (batch_size, 1)
+        # outputs
+        outputs = tf.nn.sigmoid(tf.add(wide_outputs, deep_outputs))
+        return outputs
+
+    def summary(self):
+        sparse_inputs = Input(shape=(len(self.sparse_feature_columns),), dtype=tf.int32)
+        Model(inputs=sparse_inputs, outputs=self.call(sparse_inputs)).summary()
+
+
+class DeepFMAll(object):
+    def __init__(self, dnn_feature_columns, stage, action):
         """
         :param linear_feature_columns: List of tensorflow feature_column
         :param dnn_feature_columns: List of tensorflow feature_column
         :param stage: String. Including "online_train"/"offline_train"/"evaluate"/"submit"
         :param action: String. Including "read_comment"/"like"/"click_avatar"/"favorite"/"forward"/"comment"/"follow"
         """
-        super(WideAndDeep, self).__init__()
+        super(DeepFMAll, self).__init__()
         self.num_epochs_dict = {"read_comment": 1, "like": 1, "click_avatar": 1, "favorite": 1, "forward": 1,
                                 "comment": 1, "follow": 1}
         self.estimator = None
-        self.linear_feature_columns = linear_feature_columns
         self.dnn_feature_columns = dnn_feature_columns
         self.stage = stage
         self.action = action
@@ -53,16 +177,10 @@ class WideAndDeep(object):
         elif self.stage in ["online_train", "offline_train"]:
             # 训练时如果模型目录已存在，则清空目录
             del_file(model_checkpoint_stage_dir)
-        optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, beta1=0.9, beta2=0.999,
-                                           epsilon=1)
-        config = tf.estimator.RunConfig(model_dir=model_checkpoint_stage_dir, tf_random_seed=SEED)
-        self.estimator = tf.estimator.DNNLinearCombinedClassifier(
-            model_dir=model_checkpoint_stage_dir,
-            linear_feature_columns=self.linear_feature_columns,
-            dnn_feature_columns=self.dnn_feature_columns,
-            dnn_hidden_units=[32, 8],
-            dnn_optimizer=optimizer,
-            config=config)
+        optimizer = Adam(learning_rate=FLAGS.learning_rate)
+        self.model = DeepFM(feature_columns=self.dnn_feature_columns)
+        self.model.compile(loss=binary_crossentropy, optimizer=optimizer,
+                      metrics=AUC())
 
     def df_to_dataset(self, df, stage, action, shuffle=True, batch_size=128, num_epochs=1):
         '''
@@ -106,9 +224,19 @@ class WideAndDeep(object):
                                                                        day=STAGE_END_DAY[self.stage])
         stage_dir = os.path.join(FLAGS.root_path, self.stage, file_name)
         df = pd.read_csv(stage_dir)
-        self.estimator.train(
-            input_fn=lambda: self.input_fn_train(df, self.stage, self.action, self.num_epochs_dict[self.action])
+        # self.estimator.train(
+        #     input_fn=lambda: self.input_fn_train(df, self.stage, self.action, self.num_epochs_dict[self.action])
+        # )
+        self.model.fit(
+            train_X,
+            train_y,
+            epochs=epochs,
+            callbacks=[EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)],  # checkpoint,
+            batch_size=batch_size,
+            validation_split=0.1
         )
+        # ===========================Test==============================
+        print('test AUC: %f' % model.evaluate(test_X, test_y, batch_size=batch_size)[1])
 
     def evaluate(self):
         """
@@ -175,10 +303,10 @@ def get_feature_columns():
     linear_feature_columns = list()
     # DNN features
     user_cate = fc.categorical_column_with_hash_bucket("userid", 40000, tf.int64)
-    feed_cate = fc.categorical_column_with_hash_bucket("feedid", 240000, tf.int64)
+    feed_cate = fc.categorical_column_with_hash_bucket("feedid", 40000, tf.int64)
     author_cate = fc.categorical_column_with_hash_bucket("authorid", 40000, tf.int64)
     bgm_singer_cate = fc.categorical_column_with_hash_bucket("bgm_singer_id", 40000, tf.int64)
-    bgm_song_cate = fc.categorical_column_with_hash_bucket("bgm_song_id", 60000, tf.int64)
+    bgm_song_cate = fc.categorical_column_with_hash_bucket("bgm_song_id", 40000, tf.int64)
     user_embedding = fc.embedding_column(user_cate, FLAGS.embed_dim, max_norm=FLAGS.embed_l2)
     feed_embedding = fc.embedding_column(feed_cate, FLAGS.embed_dim, max_norm=FLAGS.embed_l2)
     author_embedding = fc.embedding_column(author_cate, FLAGS.embed_dim, max_norm=FLAGS.embed_l2)
@@ -215,7 +343,8 @@ def main(argv):
     ids = None
     for action in ACTION_LIST:
         print("Action:", action)
-        model = WideAndDeep(linear_feature_columns, dnn_feature_columns, stage, action)
+        # model = DeepFM(linear_feature_columns, dnn_feature_columns, stage, action)
+        model = DeepFMAll(dnn_feature_columns, stage, action)
         model.build_estimator()
 
         if stage in ["online_train", "offline_train"]:
